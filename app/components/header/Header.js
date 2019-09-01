e99d4b4dee08397752f00407e5303d557db817a5
Nikunj Gupta <nikunj.gupta@hashedin.com>
2019-09-01 17:47:19 +0530

    header, footer, content-body file structure created

---
+++ app/components/header/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styles from './Header.css';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Header</h1>
+      </div>
+    );
+  }
+}
---
+++ app/containers/contentBody/ContentBody.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styles from './ContentBody.css';
+
+export default class ContentBody extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Content Body</h1>
+      </div>
+    );
+  }
+}
