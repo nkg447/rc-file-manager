b6b59e8a4cd172141c7ba6e88a49012f02dfb47e
nkg447 <nikunj.gupta447@gmail.com>
2019-10-19 19:17:16 +0530

    files and folder sorted

--- app/components/fileItem/FileItem.js
+++ app/components/fileItem/FileItem.js
@@ -3,34 +3,16 @@ import styles from './FileItem.css';
 import { connect } from 'react-redux';
 import { FOLDER_ICON, FILE_ICON } from '../../assets';
 
-export default (props)=>{
+export default props => {
   const { file, ...otherProps } = props;
   const isDirectory = file.isDirectory();
   const icon = isDirectory ? FOLDER_ICON : FILE_ICON;
   return (
-    <div
-      className={styles.container}
-      {...otherProps}
-    >
+    <div className={styles.container} {...otherProps}>
       <img src={icon} className={styles.icon}></img>
-      <p className={styles.name}>{file.name}</p>
+      <p className={styles.name}>
+        {file.name.substring(0, 18) + ((file.name.length > 18) ? '...' : '')}
+      </p>
     </div>
   );
-}
-
-// class FolderItem extends Component {
-
-
-//   render() {
-    
-//   }
-// }
-
-// const mapStateToProps = state => {
-//   return {};
-// };
-
-// export default connect(
-//   mapStateToProps,
-//   mapDispatchToProps
-// )(FolderItem);
+};
--- app/containers/contentBody/ContentBody.js
+++ app/containers/contentBody/ContentBody.js
@@ -16,7 +16,14 @@ class ContentBody extends Component {
 
   readDir = () => {
     const address = this.props.address;
-    const files = fs.readdirSync(address, { withFileTypes: true });
+    const files = fs
+      .readdirSync(address, { withFileTypes: true })
+      .sort((f1, f2) => {
+        if (f1.isDirectory() == f2.isDirectory()) {
+          return f1.name.localeCompare(f2.name);
+        }
+        return f1.isDirectory() ? -1 : 1;
+      });
     return files;
   };
 
@@ -33,7 +40,7 @@ class ContentBody extends Component {
           <FileItem
             key={i}
             file={file}
-            onDoubleClick={this.onDoubleClickHandler.bind(this,file)}
+            onDoubleClick={this.onDoubleClickHandler.bind(this, file)}
           ></FileItem>
         ))}
       </div>
