9e78b740539e2bc0c1928c5e244c6fe8fae3bb91
nkg447 <nikunj.gupta447@gmail.com>
2019-10-20 18:17:00 +0530

    eject icon for mounted devices

--- app/components/sidebar/ListItem.js
+++ app/components/sidebar/ListItem.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import styles from './ListItem.css';
+import { EJECT_ICON } from '../../assets';
 
-export default class ListItem extends Component {
-  render() {
-    const { onClick, address, text, ...otherProps } = this.props;
-    return (
-      <li
-        onClick={() => onClick(this.props.address)}
-        data-address={address}
-        {...otherProps}
-      >
-        {text}
-      </li>
-    );
-  }
-}
+export default props => {
+  const { item, active, text, ...otherProps } = props;
+  return (
+    <li
+      data-address={item.path}
+      className={active ? styles.activeListItem : styles.listItem}
+      {...otherProps}
+    >
+      <p>{text}</p>
+      {item.external ? (
+        <img src={EJECT_ICON} className={styles.icon}></img>
+      ) : null}
+    </li>
+  );
+};
