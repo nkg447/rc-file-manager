6639fee434bfcee90afee11d219e0469d041ce79
nkg447 <nikunj.gupta447@gmail.com>
2019-10-29 12:28:51 +0530

    added address bar to header

--- app/components/header/Header.js
+++ app/components/header/Header.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import styles from './Header.css';
-import Search from "../search/Search"
+import Search from '../search/Search';
+import AddressBar from '../addressBar/AddressBar';
 
 export default class Header extends Component {
   render() {
     return (
       <div className={styles.container}>
-        <Search keyUp={(value) => this.props.searchValueChange(value)}/>
+        <AddressBar {...this.props}></AddressBar>
+        <Search keyUp={value => this.props.searchValueChange(value)} />
       </div>
     );
   }
